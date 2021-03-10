c7e44a81f978f27add4c3d993f2320f536227597
Deen Islam <muslimbi@users.noreply.github.com>
2021-03-10 19:23:19 +0600

    create run.bat

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem python -m venv venv
+rem .\venv\Scripts\activate
+rem pip install -r requirements.txt
+rem c:\users\administrator\github\binance-tutorials\coinview\venv\scripts\python.exe -m pip install --upgrade pip
+
+.\venv\Scripts\activate
+python bot.py
+
+pause
